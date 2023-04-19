5d0914a28b719cfb72512de4cd552389622a08ed
toyman640 <falakoadebayor@gmail.com>
2023-04-19 09:54:19 +0100

    Add load more speakers feature using javascript

--- main.js
+++ main.js
@@ -24,11 +24,31 @@ function hambugerHide () {
 hamClose.addEventListener('click', hambugerHide);
 
 
-let speakSec = document.querySelector('.desk-card')
-const speakImg1 = document.querySelector('#speak-img1')
+let speakSec = document.querySelector('.desk-card');
+const speakImg1 = document.querySelector('#speak-img1');
 const speakName1 = document.querySelector('#speak-name1')
 const speakQuali1 = document.querySelector('#speak-quali1')
 const speakInfo1 = document.querySelector('#speak-info1')
+const speakImg2 = document.querySelector('#speak-img2')
+const speakName2 = document.querySelector('#speak-name2')
+const speakQuali2 = document.querySelector('#speak-quali2')
+const speakInfo2 = document.querySelector('#speak-info2')
+const speakImg3 = document.querySelector('#speak-img3')
+const speakName3 = document.querySelector('#speak-name3')
+const speakQuali3 = document.querySelector('#speak-quali3')
+const speakInfo3= document.querySelector('#speak-info3')
+const speakImg4 = document.querySelector('#speak-img4')
+const speakName4 = document.querySelector('#speak-name4')
+const speakQuali4 = document.querySelector('#speak-quali4')
+const speakInfo4 = document.querySelector('#speak-info4')
+const speakImg5 = document.querySelector('#speak-img5')
+const speakName5 = document.querySelector('#speak-name5')
+const speakQuali5 = document.querySelector('#speak-quali5')
+const speakInfo5 = document.querySelector('#speak-info5')
+const speakImg6 = document.querySelector('#speak-img6')
+const speakName6 = document.querySelector('#speak-name6')
+const speakQuali6 = document.querySelector('#speak-quali6')
+const speakInfo6 = document.querySelector('#speak-info6')
 
 const speakerObject = {
     one: {
@@ -84,5 +104,62 @@ speakImg1.src = speakerImage1;
 speakQuali1.innerHTML = speakerQuali1;
 speakInfo1.innerHTML = speakerInfo1;
 
+const speakerImage2 = speakerObject.two.image;
+const speakerName2 = speakerObject.two.name;
+const speakerQuali2 = speakerObject.two.qualifications;
+const speakerInfo2 = speakerObject.two.info;
 
+speakName2.innerHTML = speakerName2;
+speakImg2.src = speakerImage2;
+speakQuali2.innerHTML = speakerQuali2;
+speakInfo2.innerHTML = speakerInfo2;
+
+const speakerImage3 = speakerObject.three.image;
+const speakerName3 = speakerObject.three.name;
+const speakerQuali3 = speakerObject.three.qualifications;
+const speakerInfo3 = speakerObject.three.info;
+
+speakName3.innerHTML = speakerName3;
+speakImg3.src = speakerImage3;
+speakQuali3.innerHTML = speakerQuali3;
+speakInfo3.innerHTML = speakerInfo3;
+
+const speakerImage4 = speakerObject.four.image;
+const speakerName4 = speakerObject.four.name;
+const speakerQuali4 = speakerObject.four.qualifications;
+const speakerInfo4 = speakerObject.four.info;
+
+speakName4.innerHTML = speakerName4;
+speakImg4.src = speakerImage4;
+speakQuali4.innerHTML = speakerQuali4;
+speakInfo4.innerHTML = speakerInfo4;
+
+const speakerImage5 = speakerObject.five.image;
+const speakerName5 = speakerObject.five.name;
+const speakerQuali5 = speakerObject.five.qualifications;
+const speakerInfo5 = speakerObject.five.info;
+
+speakName5.innerHTML = speakerName5;
+speakImg5.src = speakerImage5;
+speakQuali5.innerHTML = speakerQuali5;
+speakInfo5.innerHTML = speakerInfo5;
+
+const speakerImage6 = speakerObject.six.image;
+const speakerName6 = speakerObject.six.name;
+const speakerQuali6 = speakerObject.six.qualifications;
+const speakerInfo6 = speakerObject.six.info;
+
+speakName6.innerHTML = speakerName6;
+speakImg6.src = speakerImage6;
+speakQuali6.innerHTML = speakerQuali6;
+speakInfo6.innerHTML = speakerInfo6;
+
+const showSpeakers = document.querySelector('#show-more');
+const otherSpeakers = document.querySelector('.play-part');
+
+function displayMore () {
+  otherSpeakers.classList.toggle('ham-gone');
+}
+
+showSpeakers.addEventListener('click', displayMore);
 
