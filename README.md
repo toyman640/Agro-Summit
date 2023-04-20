ecd6c6534085f883aea81b3be4e5fca0fe1dddc5
toyman640 <falakoadebayor@gmail.com>
2023-04-20 13:56:02 +0100

    Add logo,icon and hamburger menu to mobile about page

--- README.md
+++ README.md
@@ -1,1 +1,192 @@
-# Agro-Summit
+<a name="readme-top"></a>
+
+# :green_book: Table of Contents
+
+- [:book: About the Project](#about-project)
+  - [🛠 Built With](#built-with)
+    - [Tech Stack](#tech-stack)
+    - [Key Features](#key-features)
+  - [:rocket: Live Demo](#live-demo)
+- [:computer: Getting Started](#getting-started)
+  - [Prerequisites](#prerequisites)
+  - [Setup](#setup)
+  - [Install](#install)
+  - [Usage](#usage)
+  - [Run tests](#run-tests)
+  - [Deployment](#deployment)
+- [:bust_in_silhouette: Author](#author)
+- [:telescope: Future Features](#future-features)
+- [:handshake: Contributing](#contributing)
+- [:star:️ Show your support](#support)
+- [:pray: Acknowledgements](#acknowledgements)
+- [:question: FAQ](#faq)
+- [:memo: License](#license)
+
+# :book: Agricultural Enthusiat Global Summit project <a name="about-project"></a>
+
+**AE Global Summit project** is a  project built with HTML, CSS AND JAVASCRIPT.
+
+## 🛠 Built With <a name="built-with"></a>
+
+### Tech Stack <a name="tech-stack"></a>
+
+<details>
+  <summary>Client</summary>
+  <ul>
+    <li><a href="https://developer.mozilla.org/en-US/docs/Learn/Getting_started_with_the_web/HTML_basics">HTML5</a></li>
+    <li><a href="https://developer.mozilla.org/en-US/docs/Learn/Getting_started_with_the_web/CSS_basics">CSS3</a></li>
+    <li><a href="https://developer.mozilla.org/en-US/docs/Learn/Getting_started_with_the_web/JavaScript_basics">JAVASCRIPT</a></li>
+  </ul>
+</details>
+
+### Key Features <a name="key-features"></a>
+
+- **Responsive Design.**
+- **Pixel Perfect design.**
+- **Local Storage**
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+
+<!-- LIVE DEMO -->
+
+## 🚀 Live Demo <a name="live-demo"></a>
+
+
+- [Live Demo Link]Comming soon
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+
+## :computer: Getting Started <a name="getting-started"></a>
+
+To get a local copy up and running, follow these steps.
+
+### Prerequisites
+
+In order to run this project you need:
+
+- A web browser to view output e.g [Google Chrome](https://www.google.com/chrome/).
+- An IDE e.g [Visual studio code](https://code.visualstudio.com/).
+- [A terminal](https://code.visualstudio.com/docs/terminal/basics).
+
+### Setup
+
+Clone this repository to your desired folder or download the Zip folder:
+
+```
+git clone https://github.com/toyman640/Hello_microverse.git
+```
+
+- Navigate to the location of the folder in your machine:
+
+**``you@your-Pc-name:~$ cd Hello_microverse``**
+
+### Install
+
+Install all dependencies:
+
+```
+npm install
+```
+
+### Usage
+
+To run the project, follow these instructions:
+
+- After Cloning this repo to your local machine.
+- To get it running on your default browser and local host, run:
+```
+Open the index.html in the project directory with your preferred browser to run the project
+```
+
+### Run tests
+
+To run tests, run the following command:
+
+- Track HTML linter errors run:
+```
+npx hint .
+```
+- Track CSS linter errors run:
+```
+npx stylelint "**/*.{css,scss}"
+```
+- Track JavaScript linter errors run:
+```
+npx eslint .
+```
+- For unit testing, run:
+```
+npm test
+```
+
+### Deployment <a name="deployment"></a>
+
+You can deploy this project using: GitHub Pages,
+- I used GitHub Pages to deploy my website.
+- For more information about publishing sources, see "[About GitHub pages](https://docs.github.com/en/pages/getting-started-with-github-pages/about-github-pages#publishing-sources-for-github-pages-sites)".
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+## :bust_in_silhouette: Author <a name="author"></a>
+
+:bust_in_silhouette: **Falako Omotoyosi**
+
+- GitHub: [@toyman640](https://github.com/toyman640)
+- Twitter: [@_toyman](https://twitter.com/_toyman)
+
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+## :telescope: Future Features <a name="future-features"></a>
+
+- [ ] **Implement button to switch sort type (ascending or descending).**
+- [ ] **Implement completed and not-completed routes for completed and non-completed tasks, respectively.**
+- [ ] **Add backend to store tasks.**
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+## :handshake: Contributing <a name="contributing"></a>
+
+Contributions, issues, and feature requests are welcome!
+
+Feel free to check the [issues page](../../issues/).
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+## :star:️ Show your support <a name="support"></a>
+
+
+Give a :star:️ if you like this project and how I managed to build it!
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+## :pray: Acknowledgments <a name="acknowledgements"></a>
+
+- The original design ideal from Microverse:two_hearts:.
+- Project from [Microverse](https://bit.ly/MicroverseTN) HTML/CSS module.
+- Thanks to the Microverse team for the great curriculum.
+- Thanks to the Code Reviewer(s) for the insightful feedbacks.
+- A great thanks to My coding partner(s), morning session team, and standup team for their contributions.
+- Hat tip to anyone whose code was used.
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+## :question: FAQ <a name="faq"></a>
+
+- **Can I fork and reuse the repository**
+
+  - Yes please, feel free.
+
+- **Can I improve the repository, and my changes will be accepted if they are good?**
+
+  - Yes please, nice ideas are welcome, please.
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+## :memo: License <a name="license"></a>
+
+This project is [MIT](./LICENSE) licensed.
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
--- main.js
+++ main.js
@@ -54,112 +54,129 @@ const speakerObject = {
     one: {
         image : 'images/speaker-1.jpg',
         name : 'Samuel Benard',
-        qualifications : 'Risk analyst at International Fund for Agricultural Developmen',
-        info : '',
+        qualifications : 'Risk analyst at International Fund for Agricultural Development',
+        info : 'He worked with farmers and agricultural businesses to improve their risk management practices, providing training and consultation.',
     },
 
     two: {
-        image : 'images/speaker-1.jpg',
+        image : 'images/speaker-4.jpg',
         name : 'Akintunde Babalola',
         qualifications : 'Ph.D. in Horticulture with expertise in urban agriculture and sustainable food systems.',
-        info : '',
+        info : 'Akin published several articles on horticultural science in top-tier academic journals, which have been widely cited and contributed to advances in the field.',
     },
 
     three: {
-        image : 'images/speaker-1.jpg',
+        image : 'images/speaker5.jpg',
         name : 'Gregory Hanson',
         qualifications : 'Master of Science in Agricultural Engineering with experience in precision farming technologies.',
-        info : '',
+        info : 'He designed and implemented innovative machinery and equipment for soil preparation, planting, and harvesting,',
     },
 
     four: {
-        image : 'images/speaker-1.jpg',
+        image : 'images/speaker-2.jpg',
         name : 'Lilian Green',
         qualifications : 'Doctor of Veterinary Medicine with a specialization in large animal health and production.',
-        info : '',
+        info : 'Lilian Developed a new surgical technique for treating joint injuries in horses, resulting in faster recovery times and improved mobility for the animals',
     },
 
     five: {
-        image : 'images/speaker-1.jpg',
+        image : 'images/speaker-3.jpg',
         name : 'Mary Macaulay',
         qualifications : 'Ph.D. in Agricultural Economics with expertise in food and agricultural policy.',
-        info : ''
+        info : 'She conducted research on the impact of trade policies on agricultural production and exports'
     },
 
     six: {
-        image : 'images/speaker-1.jpg',
+        image : 'images/speaker-6.jpg',
         name : 'Kemi Anthony',
         qualifications : 'Bachelor of Science in Agricultural Communications with experience in agricultural journalism and media relations.',
-        info : '',
+        info : 'He collaborated with agricultural companies to develop effective marketing campaigns that increased sales and brand awareness.',
     }
 }
+
 
 const speakerImage1 = speakerObject.one.image;
 const speakerName1 = speakerObject.one.name;
 const speakerQuali1 = speakerObject.one.qualifications;
 const speakerInfo1 = speakerObject.one.info;
 
-speakName1.innerHTML = speakerName1;
-speakImg1.src = speakerImage1;
-speakQuali1.innerHTML = speakerQuali1;
-speakInfo1.innerHTML = speakerInfo1;
+if(location.pathname === '/Agro-Summit/index.html' ) {
+
+    speakName1.innerHTML = speakerName1;
+    speakImg1.src = speakerImage1;
+    speakQuali1.innerHTML = speakerQuali1;
+    speakInfo1.innerHTML = speakerInfo1;
+}
 
 const speakerImage2 = speakerObject.two.image;
 const speakerName2 = speakerObject.two.name;
 const speakerQuali2 = speakerObject.two.qualifications;
 const speakerInfo2 = speakerObject.two.info;
 
-speakName2.innerHTML = speakerName2;
-speakImg2.src = speakerImage2;
-speakQuali2.innerHTML = speakerQuali2;
-speakInfo2.innerHTML = speakerInfo2;
+if(location.pathname === '/Agro-Summit/index.html' ) {
+    speakName2.innerHTML = speakerName2;
+    speakImg2.src = speakerImage2;
+    speakQuali2.innerHTML = speakerQuali2;
+    speakInfo2.innerHTML = speakerInfo2;
+}
 
 const speakerImage3 = speakerObject.three.image;
 const speakerName3 = speakerObject.three.name;
 const speakerQuali3 = speakerObject.three.qualifications;
 const speakerInfo3 = speakerObject.three.info;
 
-speakName3.innerHTML = speakerName3;
-speakImg3.src = speakerImage3;
-speakQuali3.innerHTML = speakerQuali3;
-speakInfo3.innerHTML = speakerInfo3;
+if(location.pathname === '/Agro-Summit/index.html' ) {
+    speakName3.innerHTML = speakerName3;
+    speakImg3.src = speakerImage3;
+    speakQuali3.innerHTML = speakerQuali3;
+    speakInfo3.innerHTML = speakerInfo3;
+}
 
 const speakerImage4 = speakerObject.four.image;
 const speakerName4 = speakerObject.four.name;
 const speakerQuali4 = speakerObject.four.qualifications;
 const speakerInfo4 = speakerObject.four.info;
 
-speakName4.innerHTML = speakerName4;
-speakImg4.src = speakerImage4;
-speakQuali4.innerHTML = speakerQuali4;
-speakInfo4.innerHTML = speakerInfo4;
+if(location.pathname === '/Agro-Summit/index.html' ) {
+    speakName4.innerHTML = speakerName4;
+    speakImg4.src = speakerImage4;
+    speakQuali4.innerHTML = speakerQuali4;
+    speakInfo4.innerHTML = speakerInfo4;
+}
 
 const speakerImage5 = speakerObject.five.image;
 const speakerName5 = speakerObject.five.name;
 const speakerQuali5 = speakerObject.five.qualifications;
 const speakerInfo5 = speakerObject.five.info;
 
-speakName5.innerHTML = speakerName5;
-speakImg5.src = speakerImage5;
-speakQuali5.innerHTML = speakerQuali5;
-speakInfo5.innerHTML = speakerInfo5;
+if(location.pathname === '/Agro-Summit/index.html' ) {
+    speakName5.innerHTML = speakerName5;
+    speakImg5.src = speakerImage5;
+    speakQuali5.innerHTML = speakerQuali5;
+    speakInfo5.innerHTML = speakerInfo5;
+}
 
 const speakerImage6 = speakerObject.six.image;
 const speakerName6 = speakerObject.six.name;
 const speakerQuali6 = speakerObject.six.qualifications;
 const speakerInfo6 = speakerObject.six.info;
 
-speakName6.innerHTML = speakerName6;
-speakImg6.src = speakerImage6;
-speakQuali6.innerHTML = speakerQuali6;
-speakInfo6.innerHTML = speakerInfo6;
+if(location.pathname === '/Agro-Summit/index.html' ) {
+    speakName6.innerHTML = speakerName6;
+    speakImg6.src = speakerImage6;
+    speakQuali6.innerHTML = speakerQuali6;
+    speakInfo6.innerHTML = speakerInfo6;
+}
 
 const showSpeakers = document.querySelector('#show-more');
 const otherSpeakers = document.querySelector('.play-part');
 
-function displayMore () {
-  otherSpeakers.classList.toggle('ham-gone');
+if(location.pathname === '/Agro-Summit/index.html' ) {
+    function displayMore (event) {
+      otherSpeakers.classList.toggle('ham-gone');
+      event.preventDefault()
+    }
+    
+    showSpeakers.addEventListener('click', displayMore);
 }
-
-showSpeakers.addEventListener('click', displayMore);
 
