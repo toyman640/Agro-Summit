08ba3cf27ab51a4a50b48330ce285164be9e8a57
toyman640 <falakoadebayor@gmail.com>
2023-04-21 18:46:42 +0100

    Fix linters error

--- main.js
+++ main.js
@@ -16,36 +16,11 @@ const hamClose = document.querySelector('#ham-close');
 function hambugerHide() {
   popMenu.classList.toggle('ham-gone');
   for (let i = 0; i < otherSections.length; i += 1) {
-    otherSections[i].classList.toggle('ham-gone');
+    otherSections[i].classList.toggle('show-plenty');
   }
 }
 
 hamClose.addEventListener('click', hambugerHide);
-
-const speakImg1 = document.querySelector('#speak-img1');
-const speakName1 = document.querySelector('#speak-name1');
-const speakQuali1 = document.querySelector('#speak-quali1');
-const speakInfo1 = document.querySelector('#speak-info1');
-const speakImg2 = document.querySelector('#speak-img2');
-const speakName2 = document.querySelector('#speak-name2');
-const speakQuali2 = document.querySelector('#speak-quali2');
-const speakInfo2 = document.querySelector('#speak-info2');
-const speakImg3 = document.querySelector('#speak-img3');
-const speakName3 = document.querySelector('#speak-name3');
-const speakQuali3 = document.querySelector('#speak-quali3');
-const speakInfo3 = document.querySelector('#speak-info3');
-const speakImg4 = document.querySelector('#speak-img4');
-const speakName4 = document.querySelector('#speak-name4');
-const speakQuali4 = document.querySelector('#speak-quali4');
-const speakInfo4 = document.querySelector('#speak-info4');
-const speakImg5 = document.querySelector('#speak-img5');
-const speakName5 = document.querySelector('#speak-name5');
-const speakQuali5 = document.querySelector('#speak-quali5');
-const speakInfo5 = document.querySelector('#speak-info5');
-const speakImg6 = document.querySelector('#speak-img6');
-const speakName6 = document.querySelector('#speak-name6');
-const speakQuali6 = document.querySelector('#speak-quali6');
-const speakInfo6 = document.querySelector('#speak-info6');
 
 const speakerObject1 = [
   {
@@ -93,16 +68,19 @@ const speakerObject1 = [
 
 const firstPart = document.querySelector('.about-content');
 const buttonPart = document.querySelector('.more-speakers');
-const speakerCount = 3
-const speakerNum = 0
-let showMore = false
+const speakerCount = 3;
+const speakerNum = 0;
+let showMore = false;
 const speaks = [];
-for (let i = speakerNum; i < speakerObject1.length; i++) {
-  const {image, name, qualifications, info} = speakerObject1[i];
-   speaks.push({image, name, qualifications, info})
+for (let i = speakerNum; i < speakerObject1.length; i += 1) {
+  const {
+    image, name, qualifications, info,
+  } = speakerObject1[i];
+  speaks.push({
+    image, name, qualifications, info,
+  });
 
   if (speaks.length < speakerCount) {
-
     const div = document.createElement('div');
     div.classList.add('speaker-card');
     const speaksImage = document.createElement('img');
@@ -111,32 +89,27 @@ for (let i = speakerNum; i < speakerObject1.length; i++) {
     div1.classList.add('speaker-info');
     const speaksName = document.createElement('h5');
     speaksName.classList.add('speaker-name');
-    const speaksQ = document.createElement('p')
-    speaksQ.classList.add('speaker-qualification','main-color');
-    const hrL = document.createElement('hr')
-    hrL.classList.add('speaker-hr')
-    const speaksInfo = document.createElement('p')
+    const speaksQ = document.createElement('p');
+    speaksQ.classList.add('speaker-qualification', 'main-color');
+    const hrL = document.createElement('hr');
+    hrL.classList.add('speaker-hr');
+    const speaksInfo = document.createElement('p');
     speaksInfo.classList.add('small-text');
-  
-    speaksImage.src = speakerObject1[i].image
-    speaksName.textContent = speakerObject1[i].name
-    speaksQ.textContent = speakerObject1[i].qualifications
-    speaksInfo.textContent = speakerObject1[i].info
-  
+    speaksImage.src = speakerObject1[i].image;
+    speaksName.textContent = speakerObject1[i].name;
+    speaksQ.textContent = speakerObject1[i].qualifications;
+    speaksInfo.textContent = speakerObject1[i].info;
     div.appendChild(speaksImage);
     div.appendChild(div1);
     div1.appendChild(speaksName);
     div1.appendChild(speaksQ);
     div1.appendChild(hrL);
     div1.appendChild(speaksInfo);
-  
-    firstPart.appendChild(div)
-    buttonPart.parentNode.insertBefore(div, buttonPart)
-  }
-  else {
-    
-    const div = document.createElement('div')
-    div.classList.add('show-plenty')
+    firstPart.appendChild(div);
+    buttonPart.parentNode.insertBefore(div, buttonPart);
+  } else {
+    const div = document.createElement('div');
+    div.classList.add('show-plenty');
     div.classList.add('speaker-card');
     const speaksImage = document.createElement('img');
     speaksImage.classList.add('speaker-image');
@@ -144,44 +117,37 @@ for (let i = speakerNum; i < speakerObject1.length; i++) {
     div1.classList.add('speaker-info');
     const speaksName = document.createElement('h5');
     speaksName.classList.add('speaker-name');
-    const speaksQ = document.createElement('p')
-    speaksQ.classList.add('speaker-qualification','main-color');
-    const hrL = document.createElement('hr')
-    hrL.classList.add('speaker-hr')
-    const speaksInfo = document.createElement('p')
+    const speaksQ = document.createElement('p');
+    speaksQ.classList.add('speaker-qualification', 'main-color');
+    const hrL = document.createElement('hr');
+    hrL.classList.add('speaker-hr');
+    const speaksInfo = document.createElement('p');
     speaksInfo.classList.add('small-text');
-  
-    speaksImage.src = speakerObject1[i].image
-    speaksName.textContent = speakerObject1[i].name
-    speaksQ.textContent = speakerObject1[i].qualifications
-    speaksInfo.textContent = speakerObject1[i].info
-  
+    speaksImage.src = speakerObject1[i].image;
+    speaksName.textContent = speakerObject1[i].name;
+    speaksQ.textContent = speakerObject1[i].qualifications;
+    speaksInfo.textContent = speakerObject1[i].info;
     div.appendChild(speaksImage);
     div.appendChild(div1);
     div1.appendChild(speaksName);
     div1.appendChild(speaksQ);
     div1.appendChild(hrL);
     div1.appendChild(speaksInfo);
-  
-    firstPart.appendChild(div)
-    buttonPart.parentNode.insertBefore(div, buttonPart)
+    firstPart.appendChild(div);
+    buttonPart.parentNode.insertBefore(div, buttonPart);
   }
-  
-
-
 }
 
 const moreButton = document.querySelector('#show-more');
-function showRest () {
+function showRest() {
   showMore = !showMore;
 
   if (showMore) {
-    const reveal = firstPart.querySelectorAll('.show-plenty')
-
-    reveal.forEach(function(element) {
-      element.classList.toggle('show-plenty')
-    })
+    const reveal = firstPart.querySelectorAll('.show-plenty');
+    reveal.forEach((element) => {
+      element.classList.toggle('show-plenty');
+    });
   }
 }
 
-moreButton.addEventListener('click', showRest)
+moreButton.addEventListener('click', showRest);
